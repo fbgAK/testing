931b325d541b35a409fde61c51be802d249d2683
Akash <147472633+fbgAK@users.noreply.github.com>
2023-12-01 17:07:08 -0700

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let data;
+
+
+
+ 
+ 
+</script>
+
+
+
+<h1>Hello World </h1>
+<h2>Hope this deploys</h2>
+<h3>{data.blog.id}</h3>
+<h1>{data.blog.date_created}</h1>
+<h1>{data.blog.tags}</h1>
+<h1>{data.blog.body}</h1>
+<h1>{data.blog.summary}</h1>
+<h1>{data.blog.date}</h1>
+<h1>{data.blog.author}</h1>
+<h1>{data.blog.title}</h1>
+
+
+
+
